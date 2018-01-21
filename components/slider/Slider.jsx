fcaa74e329c9a920f2c960a28d33f655261e2361
Xiaofeng Li <xlics05@gmail.com>
2018-01-20 23:08:29 -0500

    add new dir

---
+++ components/slider/Slider.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Slider extends Component {
+   render() {
+      return (
+         <div>
+            <h2>put your code</h2>
+         </div>
+      );
+   }
+}
+export default Slider;
